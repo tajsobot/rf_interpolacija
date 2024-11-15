4524af033e5c86b98a2ee40887f884afb2783753
Taj <taj.sobot@gmail.com>
2024-11-15 15:20:39 +0100

    lg

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
-double lgInterpolacija(const vector<vector<int>> points, double x) {
+double lgInterpolacija(const vector<vector<double>> points, double x) {
     double sum = 0.0;
     int n = points.size();
 
@@ -20,32 +21,55 @@ double lgInterpolacija(const vector<vector<int>> points, double x) {
     return sum;
 }
 
-double nwInterpolacija(const vector<vector<int>> points, double x) {
+//ne dela :(
+double nwInterpolacija(const vector<vector<double>> points, double x) {
     double sum = 0.0;
     int n = points.size();
+
+    float a[] = {1.0, 2, 7/13.0};
 
     for (int i = 0; i < n; i++) {
         //logika za a???
 
-        double currA = 0;
-        sum += currA * (x - points[i][0]);
+        sum += a[i]* (x - points[i][0]);
     }
+
     return sum;
 }
 
 int main() {
-    vector<vector<int>> podatki = {{-1, 1}, {0, 0}, {2, 4}};
-    // double x1;
-    // cout << "vnesi x: " << endl;
-    // cin >> x1;
-    // double vrednostP = lgInterpolacija(podatki, x1);
-    // cout << "Pri x = " << x1 << endl << "je P(x) = " << vrednostP << endl << endl;
 
-    double x2;
-    cout << "vnesi x (newtnovo): " << endl;
-    cin >> x2;
-    double vrednostN = nwInterpolacija(podatki, x2);
-    cout << "Pri x = " << x2 << endl << "je P(x) = " << vrednostN << endl;
+    //~~~~~~ vhodni podatki ~~~~~~~//
+    vector<vector<double>> podatki = {{-1, 1}, {0, 0}, {1, 1}};
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+    vector<vector<double>> out;
+    double min_v = -20.0;
+    double max_v = 20.0;
+    double step = 0.1;
+
+    cout << "min: " << endl;
+    cin >> min_v;
+    cout << "max: "<< endl;
+    cin >> max_v;
+    cout << "step: "<< endl;
+    cin >> step;
+
+    for (double i = min_v; i <= max_v; i += step ) {
+        double y = lgInterpolacija(podatki, i);
+        out.push_back({i, y});
+    }
+    cout << "izracun naret!" << endl;
+
+    std::ofstream file("output.dat");
+
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file for writing!" << std::endl;
+        return 1;
+    }
+    for (const auto& point : out) {
+        file << point[0] << " " << point[1] << "\n";
+    }
 
     return 0;
 }
